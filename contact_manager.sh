de725227f2bc9d9796e94b0c57a91ed562a0b237
John Q Julien <johnqjulien@gmail.com>
2025-05-12 21:47:23 -0500

    Add search functionality to contact manager CLI tool

--- contact_manager.sh
+++ contact_manager.sh
@@ -16,9 +16,10 @@ echo "Choose an action:"
 echo "1. Add Contact"
 echo "2. View All Contacts"
 echo "3. Delete Contact"
+echo "4. Search Contact"
 
 # Get user's choice
-read -p "Enter option (1/2/3): " choice
+read -p "Enter option (1/2/3/4): " choice
 
 # Decide what to do
 case $choice in
@@ -37,6 +38,11 @@ case $choice in
         read -p "Enter name of contact to delete: " delname
         sqlite3 $DB_NAME "DELETE FROM contacts WHERE name='$delname';"
         echo "Contact deleted (if found)."
+        ;;
+    4)
+        read -p "Enter name to search: " search_term
+        echo "Search results:"
+        sqlite3 $DB_NAME "SELECT * FROM contacts WHERE name LIKE '%$search_term%';"
         ;;
     *)
         echo "Invalid choice."
